e0086ef428434df568c4b5ead14418bc9f6099bf
Krzysztof Woźniczak <krzysztofautomatyk@gmail.com>
2020-03-17 19:56:30 +0100

    START

---
+++ ASP.NET-CORE-MVC-Bulky-Book/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_CORE_MVC_Bulky_Book.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
